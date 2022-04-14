1ff4a2d8f3891b6a5b157157985eed0d8c1f79e9
Shri_pradhan0807 <shrikant.pradhan@globallogic.com>
2022-04-14 23:57:19 +0530

    Initial commit

---
+++ src/main/java/com/shritech/gms/GmsVehicleMsApplication.java
@@ -0,0 +1,13 @@
+package com.shritech.gms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmsVehicleMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmsVehicleMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shritech/gms/GmsVehicleMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shritech.gms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmsVehicleMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
